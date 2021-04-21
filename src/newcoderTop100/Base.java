7418314e187a32e5708a2ccb6b32f5da6d14c838
袁华健 <mars@xingren.com>
2021-04-21 11:24:05 +0800

    feat:两个链表生成相加链表      反转字符串      在二叉树中找到两个节点的最近公共祖先      螺旋矩阵

--- src/newcoderTop100/Base.java
+++ src/newcoderTop100/Base.java
@@ -24,10 +24,35 @@ public class Base {
             var colStr = row[i];
             var split = colStr.split(",");
             var list = Arrays.stream(split).filter(this::isNotBlank).map(Integer::parseInt).collect(Collectors.toList());
-            matrix[i][0] = list.get(0);
-            matrix[i][1] = list.get(1);
+            for (int j = 0; j < list.size(); j++) {
+                matrix[i][j] = list.get(j);
+            }
         }
         return matrix;
+    }
+
+    public ListNode toList(String template) {
+        ListNode head = new ListNode(0);
+        ListNode pre = head;
+        var split = template.split(",");
+        for (int i = 0; i < split.length; i++) {
+            ListNode cur = new ListNode(Integer.parseInt(split[i]));
+            pre.next = cur;
+            pre = cur;
+        }
+        return head.next;
+    }
+
+    protected void printListNode(ListNode node) {
+        if (node == null) {
+            System.out.println("null");
+            return;
+        }
+        while (node.next != null) {
+            System.out.print(node.val + " -> ");
+            node = node.next;
+        }
+        System.out.print(node.val);
     }
 
     private boolean isNotBlank(String s) {
