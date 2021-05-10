9f3f341dff6a3d68b18ceca77b5d416092c05041
袁华健 <mars@xingren.com>
2021-05-10 15:45:34 +0800

    基本计算器.java 逆波兰表达式求值.java

--- src/leetcode/editor/cn/基本计算器.java
+++ src/leetcode/editor/cn/基本计算器.java
@@ -52,7 +52,6 @@ public class 基本计算器 {
 
         class Res {
             int res;
-
             int right;
 
             public Res(int res, int right) {
@@ -66,7 +65,7 @@ public class 基本计算器 {
          * @return
          */
         public int calculate(String s) {
-            String trim = s.trim();
+            String trim = s.replaceAll(" ", "");
             try {
                 return Integer.parseInt(trim);
             } catch (Exception e) {
@@ -84,9 +83,15 @@ public class 基本计算器 {
             int right = -1;
             for (int i = left; i < c.length; i++) {
                 char n = c[i];
-                if (n >= '0' && n <= '9') {
-                    nums.offer(n - '0');
-                } else if (n == ' ') {
+                int last = isNumber(c, i);
+                if (i == left && n == '-') {
+                    //第一个值为负数 - ，加上一个0，方便计算
+                    nums.offer(0);
+                    ops.offer('-');
+                } else if (last != -1) {
+                    //如果是数字
+                    nums.offer(getNumber(c, i, last));
+                    i = last;
                 } else if (n == ')') {
                     //遇到括号就停止
                     right = i;
@@ -99,22 +104,54 @@ public class 基本计算器 {
                     ops.offer(n);
                 }
             }
-
             //初始化完成，计算结果。
             while (!ops.isEmpty()) {
                 Integer p1 = nums.poll();
                 Integer p2 = nums.poll();
                 Character op = ops.poll();
                 Integer res = null;
-                if (op == '+') {
-                    res = p1 + p2;
-                } else if (op == '-') {
-                    res = p1 - p2;
+                if (p1 != null && p2 != null) {
+                    if (op == '+') {
+                        res = p1 + p2;
+                    } else if (op == '-') {
+                        res = p1 - p2;
+                    }
+                } else {
+                    //多出来一个负数
+                    if (op == '+') {
+                        res = p1;
+                    } else if (op == '-') {
+                        res = -p1;
+                    }
                 }
+
                 //todo 乘法
                 nums.addFirst(res);
             }
             return new Res(nums.poll(), right);
+        }
+
+        private Integer getNumber(char[] c, int i, int last) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = i; j <= last; j++) {
+                sb.append(c[j]);
+            }
+            return Integer.parseInt(sb.toString());
+        }
+
+
+        private int isNumber(char[] c, int index) {
+            //是不是数字，如果是数字就返回最后一个坐标的下标，不是就返回-1
+            int last = -1;
+            for (int i = index; i < c.length; i++) {
+                char n = c[i];
+                if (n >= '0' && n <= '9') {
+                    last = i;
+                } else {
+                    break;
+                }
+            }
+            return last;
         }
     }
 
@@ -123,7 +160,11 @@ public class 基本计算器 {
     public static void main(String[] args) {
         Solution solution = new 基本计算器().new Solution();
 //        Solution.Res cal = solution.cal(new char[]{'1', '-', '2', '+', '3'}, 0);
-        int cal = solution.calculate("-2+ 1");
+
+//        int cal = solution.calculate("-2+ 1");
+//        int cal = solution.calculate("- (3 + (4 + 5))");
+        int cal = solution.calculate("1-(+1+1)");
+//        int cal = solution.calculate("1-11");
         System.out.println(cal);
     }
 
