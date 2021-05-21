d57369aa40990691ce017d52ed1befee619c2aea
袁华健 <mars@xingren.com>
2021-05-21 14:08:24 +0800

    字符串的排列.java

--- src/leetcode/editor/cn/字符串的排列.java
+++ src/leetcode/editor/cn/字符串的排列.java
@@ -1,85 +1,101 @@
-//输入一个字符串，打印出该字符串中字符的所有排列。 
+//给定两个字符串 s1 和 s2，写一个函数来判断 s2 是否包含 s1 的排列。 
+//
+// 换句话说，第一个字符串的排列之一是第二个字符串的 子串 。 
 //
 // 
 //
-// 你可以以任意顺序返回这个字符串数组，但里面不能有重复元素。 
+// 示例 1： 
+//
+// 
+//输入: s1 = "ab" s2 = "eidbaooo"
+//输出: True
+//解释: s2 包含 s1 的排列之一 ("ba").
+// 
+//
+// 示例 2： 
+//
+// 
+//输入: s1= "ab" s2 = "eidboaoo"
+//输出: False
+// 
 //
 // 
 //
-// 示例: 
-//
-// 输入：s = "abc"
-//输出：["abc","acb","bac","bca","cab","cba"]
-// 
+// 提示： 
 //
 // 
-//
-// 限制： 
-//
-// 1 <= s 的长度 <= 8 
-// Related Topics 回溯算法 
-// 👍 256 👎 0
+// 输入的字符串只包含小写字母 
+// 两个字符串的长度都在 [1, 10,000] 之间 
+// 
+// Related Topics 双指针 Sliding Window 
+// 👍 351 👎 0
 
 package leetcode.editor.cn;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * [剑指 Offer 38] 字符串的排列 </br>
+ * [567] 字符串的排列 </br>
  *
- * @see <a href="https://leetcode-cn.com/problems/zi-fu-chuan-de-pai-lie-lcof/">字符串的排列</a>
+ * @see <a href="https://leetcode-cn.com/problems/permutation-in-string/">字符串的排列</a>
  */
 public class 字符串的排列 {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-
-        Set<String> result;
-
-        public String[] permutation(String s) {
-            char[] chars = s.toCharArray();
-            boolean[] map = new boolean[s.length()];
-            result = new HashSet<>();
-            backtracking("", map, chars);
-            return result.toArray(String[]::new);
-        }
-
-        /**
-         * 思路，回溯算法，列出所有的可能。
-         * 回溯算法的框架，
-         * 状态：i
-         * 选择：
-         * 撤销选择：
-         *
-         * @return
-         */
-        public void backtracking(String roads, boolean[] map, char[] arrays) {
-            if (roads.length() == arrays.length) {
-                //走到头了
-                result.add(roads);
-                return;
+        public boolean checkInclusion(String s1, String s2) {
+            int left = 0;
+            int right = 0;
+            int valid = 0;
+            Map<Character, Integer> window = new HashMap<>();
+            Map<Character, Integer> need = new HashMap<>();
+            for (int i = 0; i < s1.length(); i++) {
+                char c = s1.charAt(i);
+                need.put(c, need.getOrDefault(c, 0) + 1);
             }
-            //穷举当前的所有选择。
-            for (int i = 0; i < arrays.length; i++) {
-                if (map[i]) {
-                    //如果已经出现过，则跳过
-                    continue;
+
+            while (right < s2.length()) {
+                //
+                char rightC = s2.charAt(right);
+                //
+                if (need.containsKey(rightC)) {
+                    //如果当前字符在需要的字符集中
+                    //abacad, ca
+                    window.put(rightC, window.getOrDefault(rightC, 0) + 1);
+                    //TODO
+                    if (window.get(rightC).equals(need.get(rightC))) {
+                        valid++;
+                    }
                 }
-                map[i] = true;
-                //选择，加上新的字符
-                backtracking(roads + (arrays[i]), map, arrays);
-                //撤销选择，roads保持原样
-                map[i] = false;
+                //判断窗口是否可以收缩，收缩的时机是当前窗口的长度是否等于s1的长度，如果大于就要移除left
+                //abacad, ca
+                if (right - left + 1 == s1.length()) {
+                    //如果当前窗口的valid数量等于need，就返回true。
+                    if (valid == need.size()) {
+                        return true;
+                    }
+                    char leftC = s2.charAt(left);
+                    left++; //收缩窗口
+                    if (need.containsKey(leftC)) {
+                        //如果当前字符在需要的字符集中
+                        //abacad, ca
+                        if (window.get(leftC).equals(need.get(leftC))) {
+                            valid--;
+                        }
+                        window.put(leftC, window.getOrDefault(leftC, 0) - 1);
+                    }
+                }
+                right++;
             }
+            return false;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
     public static void main(String[] args) {
         Solution solution = new 字符串的排列().new Solution();
-        System.out.println(Arrays.toString(solution.permutation("aab")));
+        solution.checkInclusion("ab", "aob");
     }
 
 }
